ca06a92ac396630e13dc0d11f0de1d5692493908
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-05-01 12:57:22 +0300

    Add name validation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 const form = document.querySelector('.form');
-const firstNameInput = document.querySelector('#first-name');
+const firstNameInput = form.querySelector('#first-name');
+const lastNameInput = form.querySelector('#last-name');
 const passwordInput = form.querySelector('#password');
 const repeatPasswordInput = form.querySelector('#confirm-password');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
+
+firstNameInput.addEventListener('input', handleInput);
+lastNameInput.addEventListener('input', handleInput);
+
+function handleInput(e) {
+  const spanFirstName = form.querySelector(`#${e.target.id} + span`);
+
+  if (e.target.value.trim() === '') {
+    spanFirstName.innerHTML =
+      'Cannot be empty! <i class="fa-solid fa-triangle-exclamation"></i>';
+    spanFirstName.style.color = 'var(--primary-light)';
+    spanFirstName.style.backgroundColor = 'var(--invalid-color)';
+  } else {
+    spanFirstName.innerHTML = '<i class="fa-solid fa-check"></i>';
+    spanFirstName.style.color = 'var(--primary-light)';
+    spanFirstName.style.backgroundColor = 'var(--valid-color)';
+  }
+}
 
 repeatPasswordInput.addEventListener('input', (e) => {
   const spanConfirmPassword = form.querySelector('#confirm-password + span');
