5e43496b87126f7b1f9332ac0be5e9e529284d6b
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-05-01 13:05:33 +0300

    Add email validation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.form');
 const firstNameInput = form.querySelector('#first-name');
 const lastNameInput = form.querySelector('#last-name');
+const emailInput = form.querySelector('#email');
 const passwordInput = form.querySelector('#password');
 const repeatPasswordInput = form.querySelector('#confirm-password');
 
@@ -10,19 +11,36 @@ form.addEventListener('submit', (e) => {
 
 firstNameInput.addEventListener('input', handleInput);
 lastNameInput.addEventListener('input', handleInput);
+emailInput.addEventListener('input', handleEmail);
 
 function handleInput(e) {
-  const spanFirstName = form.querySelector(`#${e.target.id} + span`);
+  const spanName = form.querySelector(`#${e.target.id} + span`);
 
   if (e.target.value.trim() === '') {
-    spanFirstName.innerHTML =
+    spanName.innerHTML =
       'Cannot be empty! <i class="fa-solid fa-triangle-exclamation"></i>';
-    spanFirstName.style.color = 'var(--primary-light)';
-    spanFirstName.style.backgroundColor = 'var(--invalid-color)';
+    spanName.style.color = 'var(--primary-light)';
+    spanName.style.backgroundColor = 'var(--invalid-color)';
   } else {
-    spanFirstName.innerHTML = '<i class="fa-solid fa-check"></i>';
-    spanFirstName.style.color = 'var(--primary-light)';
-    spanFirstName.style.backgroundColor = 'var(--valid-color)';
+    spanName.innerHTML = '<i class="fa-solid fa-check"></i>';
+    spanName.style.color = 'var(--primary-light)';
+    spanName.style.backgroundColor = 'var(--valid-color)';
+  }
+}
+
+function handleEmail(e) {
+  const spanEmail = form.querySelector('#email + span');
+
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (e.target.value.match(regex)) {
+    spanEmail.innerHTML = '<i class="fa-solid fa-check"></i>';
+    spanEmail.style.color = 'var(--primary-light)';
+    spanEmail.style.backgroundColor = 'var(--valid-color)';
+  } else {
+    spanEmail.innerHTML =
+      'Invalid email! <i class="fa-solid fa-triangle-exclamation"></i>';
+    spanEmail.style.color = 'var(--primary-light)';
+    spanEmail.style.backgroundColor = 'var(--invalid-color)';
   }
 }
 
